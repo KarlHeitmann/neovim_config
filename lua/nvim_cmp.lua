d48ed2377f0fdffa0a7ed89bf5173fe76929c154
Karl Heitmann <khf1988@gmail.com>
2023-04-26 09:47:25 -0400

    fixed capabilities to use default_capabilities

--- lua/nvim_cmp.lua
+++ lua/nvim_cmp.lua
@@ -1,7 +1,9 @@
 -- nvim cmp
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- capabilities = require('cmp_nvim_lsp').default_capabilities
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
