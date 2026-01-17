9bd7591fbe8c5418f3e11692c1d30dc0f86e0812
Karl Heitmann Frutuoso <karl.frutuoso@signinsolutions.com>
2026-01-17 16:10:27 -0300

    enhances telescope bindings, adds new plugins: octo, git-rubharb angular

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,26 +33,35 @@ return {
       pcall(require("telescope").load_extension, "fzf")
 
       local map = require("helpers.keys").map
-      map("n", "<space>fr", require("telescope.builtin").oldfiles, "Recently opened")
+      -- map("n", "<space>fr", require("telescope.builtin").oldfiles, "Recently opened")
+      map("n", "<space>fr", function()
+        require("telescope.builtin").oldfiles({ only_cwd = true })
+      end, "Recently opened")
       -- map("n", "<space><space>", require("telescope.builtin").buffers, "Open buffers")
       map("n", "<space>sb", require("telescope.builtin").buffers, "Open buffers")
-      map("n", "<space>/", function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-          winblend = 10,
-          previewer = false,
-        }))
-      end, "Search in current buffer")
+      map("n", "<space>/", require("telescope.builtin").current_buffer_fuzzy_find, "Search in current buffer")
+      -- map("n", "<space>/", function()
+      --   -- You can pass additional configuration to telescope to change theme, layout, etc.
+      --   -- require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+      --   --   winblend = 10,
+      --   --   previewer = true,
+      --   -- }))
+      --   require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+      --     -- winblend = 10,
+      --     previewer = true,
+      --   }))
+      -- end, "Search in current buffer")
 
       map("n", "<space>sf", require("telescope.builtin").find_files, "Files")
       map("n", "<space>sh", require("telescope.builtin").help_tags, "Help")
       map("n", "<space>sw", require("telescope.builtin").grep_string, "Current word")
+      map("v", "<space>sw", require("telescope.builtin").grep_string, "Current word")
       map("n", "<space>sg", require("telescope.builtin").live_grep, "Grep")
       map("n", "<space>sd", require("telescope.builtin").diagnostics, "Diagnostics")
 
       -- vim.keymap.set("n", "<space>en", function()
       --   require('telescope.builtin').find_files {
-      --     cwd = vim.fn.
+      --     cwd = vim.fns
       --   }
       -- end)
 
