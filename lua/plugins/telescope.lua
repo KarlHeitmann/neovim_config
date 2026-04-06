804f371114f1a00837133312dc962e56ebf64402
Karl Heitmann Frutuoso <karl.frutuoso@signinsolutions.com>
2026-04-06 13:56:26 -0500

    fixes all deprecation warnings and updates telescope to latest release

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     -- dir = "/home/karl/github/KarlHeitmann/nvim_plugins/telescope.nvim",
-    branch = "0.1.x",
+    -- branch = "0.2.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
